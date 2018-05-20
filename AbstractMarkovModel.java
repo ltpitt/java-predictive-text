e957edca2958584f779d35cd7dc8d7a4c87a453f
ltpitt <d.nastri@gmail.com>
2018-05-20 22:37:17 +0200

    feat: Added seed to AbstractMarkovModel and ImarkovModel

--- AbstractMarkovModel.java
+++ AbstractMarkovModel.java
@@ -9,17 +9,22 @@
 import java.util.*;
 
 public abstract class AbstractMarkovModel implements IMarkovModel {
+
     protected String myText;
     protected Random myRandom;
-    
+
     public AbstractMarkovModel() {
         myRandom = new Random();
     }
-    
+
+    public void setRandom(int seed) {
+        myRandom.setSeed(seed);
+    }
+
     public void setTraining(String s) {
         myText = s.trim();
     }
- 
+
     abstract public String getRandomText(int numChars);
 
 
